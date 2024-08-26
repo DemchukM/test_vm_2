99c27cd911a347a8eb742bde15e65934bfd60b8e
Demchuk Mukola <102595539+DemchukM@users.noreply.github.com>
2024-08-26 21:15:29 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 export default async function test() {
   const data = await fetch('https://api.github.com/repos/DemchukM/invika/contents/my_checkbox?ref=main');
-  log(data.text());
+  log(await data.text());
 }
