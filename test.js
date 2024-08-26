79a2b7570125b758b5e6c911a13424d8c2f70da5
Demchuk Mykola <kolya233445@gmail.com>
2024-08-26 21:10:57 +0300

    w

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-export default function test() {
-  log('test');
+export default async function test() {
+  const data = await fetch('https://api.github.com/repos/DemchukM/invika/contents/my_checkbox?ref=main');
+  log(data.text());
 }
