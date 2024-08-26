303e853cd8ab7bc4be94dda51cddd4ed2d610b87
Demchuk Mukola <102595539+DemchukM@users.noreply.github.com>
2024-08-26 21:29:14 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 export default async function test() {
   log('Початок асинхронного виконання');
-            const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-            log('Отримано дані:', response.data);
+            const data = await axiosGet('https://jsonplaceholder.typicode.com/todos/1');
+            log('Отримано дані:', data);
             log('Кінець асинхронного виконання');
 }
